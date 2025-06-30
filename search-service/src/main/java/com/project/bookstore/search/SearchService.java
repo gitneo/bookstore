40b7038c84f8184160a4a2361122f9c96e9d33d2
Ogo Okafor <ogookafor@gmail.com>
2025-06-30 20:12:57 +0200

    GENAI=NO Create main class

---
+++ search-service/src/main/java/com/project/bookstore/search/SearchService.java
@@ -0,0 +1,10 @@
+package com.project.bookstore.search;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchService {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SearchService.class, args);
+    }
+}
